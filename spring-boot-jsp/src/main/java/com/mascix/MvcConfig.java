eebac2a6af7b3bedab222f6e6cf589044c235a60
ozkanpakdil <ozkan.pakdil@gmail.com>
2019-10-19 12:25:30 +0200

    spring boot 2 jsp add external folder for resources

---
+++ spring-boot-jsp/src/main/java/com/mascix/MvcConfig.java
@@ -0,0 +1,27 @@
+package com.mascix;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/test/**")
+                .addResourceLocations("file:D://tmp2/");
+    }
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/jsp/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+}
---
+++ spring-boot-jsp/src/main/webapp/WEB-INF/jsp/hello.jsp
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello ${name}!</title>
+</head>
+<body>
+    <h2 class="hello-title">Hello ${name}!</h2>
+    <img src="/test/1.svg.png" alt="test"/>
+</body>
+</html>
