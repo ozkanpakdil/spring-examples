6713e21bbd92dd3652de1d6d9a4b82915fc123ae
Özkan Pakdil <ozkan.pakdil@gmail.com>
2023-11-05 15:51:22 +0000

    testing https://stackoverflow.com/questions/77415986/task-nativetest-failed

---
+++ nativetest/src/main/java/com/example/nativetest/NativetestApplication.java
@@ -0,0 +1,13 @@
+package com.example.nativetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NativetestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NativetestApplication.class, args);
+	}
+
+}
---
+++ nativetest/src/test/java/com/example/nativetest/NativetestApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.nativetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NativetestApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assert 2 * 2 == 4;
+    }
+
+}
